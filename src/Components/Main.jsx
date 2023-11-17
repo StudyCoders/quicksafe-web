c2890e1ee8f4c4b61dbfd80112b96f343242b1a9
caioarai <caio.arai8@gmail.com>
2023-11-16 22:56:40 -0300

    Adicionado campos das informções de um contato

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,25 +4,26 @@ import TxtForm from '../Forms/TxtForm';
 import MapCoordinates from './MapCoordinates';
 
 const Main = () => {
+  const [contato, setContato] = React.useState(null);
   const testCoordinates = [
     {
-      "lat": -22.336242338753255,
-      "lng": -49.06343452478047
+      lat: -22.336242338753255,
+      lng: -49.06343452478047,
     },
     {
-      "lat": -22.3299077,
-      "lng": -49.0739861
+      lat: -22.3299077,
+      lng: -49.0739861,
     },
     {
-      "lat": -22.3247475,
-      "lng": -49.0327961
-    }
-  ]
+      lat: -22.3247475,
+      lng: -49.0327961,
+    },
+  ];
 
   return (
     <section className="animeLeft">
       <div className="container">
-        <div className="row mb-4">
+        <div className="row mb-5">
           <label htmlFor="selectBusca" className="fw-bold">
             Pesquise pelo{' '}
             <span className="text-decoration-underline">Código do usuário</span>
@@ -49,77 +50,106 @@ const Main = () => {
           </div>
         </div>
 
-        <h2 style={{ color: '#666' }}>Informações pessoais</h2>
-        <div className="row mb-3">
-          <div className="col-4">
-            <Text titulo="Código do Usuário:" txt="117" />
+        <div className="col">
+          <h2 style={{ color: '#666' }}>Informações pessoais</h2>
+          <div className="row mb-3">
+            <div className="col-4">
+              <Text titulo="Código do Usuário:" txt="117" />
+            </div>
+            <div className="col-4">
+              <Text titulo="Nome do usuário:" txt="CAIO TESTE" />
+            </div>
+            <div className="col-4">
+              <Text titulo="E-mail:" txt="caio@gmail.com" />
+            </div>
           </div>
-          <div className="col-4">
-            <Text titulo="Nome do usuário:" txt="CAIO TESTE" />
-          </div>
-          <div className="col-4">
-            <Text titulo="E-mail:" txt="caio@gmail.com" />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-4">
-            <Text titulo="CPF:" txt="000.111.720-00" />
-          </div>
-          <div className="col-4">
-            <Text titulo="Data de Nascimento:" txt="28/06/2002" />
-          </div>
-          <div className="col-4">
-            <Text titulo="Sexo:" txt="Masculino" />
-          </div>
-        </div>
-
-        <hr />
-
-        <h2 style={{ color: '#666' }}>Endereços</h2>
-        <div className="row mb-3">
-          <div className="col-3">
-            <Text titulo="Telefone:" txt="-" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Celular:" txt="(11) 99999-9999" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Estado:" txt="SÃO PAULO" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Cidade:" txt="BAURU" />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-3">
-            <Text titulo="CEP:" txt="17052-080" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Endereço:" txt="Av. Getúlio Vargas, 10-130" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Bairro:" txt="Jardim América" />
-          </div>
-          <div className="col-3">
-            <Text titulo="Complemento:" txt="Restaurante" />
+          <div className="row mb-3">
+            <div className="col-4">
+              <Text titulo="CPF:" txt="000.111.720-00" />
+            </div>
+            <div className="col-4">
+              <Text titulo="Data de Nascimento:" txt="28/06/2002" />
+            </div>
+            <div className="col-4">
+              <Text titulo="Sexo:" txt="Masculino" />
+            </div>
           </div>
         </div>
 
         <hr />
 
-        <h2 style={{ color: '#666' }}>Formulário</h2>
-        <TxtForm titulo="Plano de saúde:" txt="UNIMED" />
-        <TxtForm titulo="Possui algum tipo de alergia?" txt="Não" />
-        <TxtForm titulo="Toma algum medicamento contínuo?" txt="Não" />
-        <TxtForm
-          titulo="Já realizou alguma cirurgia?"
-          txt="Sim, em 2010 já realizei uma cirurgia bal bla bla da Silva teste"
-        />
-        <TxtForm titulo="Comorbidade:" txt="DOENÇA RENAL CRÔNICA" />
+        {contato && (
+          <div className="col">
+            <h2 style={{ color: '#666' }}>Informações do Contato</h2>
+            <div className="row mb-3">
+              <div className="col-4">
+                <Text titulo="Código do Contato:" txt="2" />
+              </div>
+              <div className="col-4">
+                <Text titulo="Nome do contato:" txt="MÃE" />
+              </div>
+              <div className="col-4">
+                <Text titulo="CPF:" txt="-" />
+              </div>
+            </div>
+
+            <hr />
+          </div>
+        )}
+
+        <div className="col">
+          <h2 style={{ color: '#666' }}>Endereços</h2>
+          <div className="row mb-3">
+            <div className="col-3">
+              <Text titulo="Telefone:" txt="-" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Celular:" txt="(11) 99999-9999" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Estado:" txt="SÃO PAULO" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Cidade:" txt="BAURU" />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-3">
+              <Text titulo="CEP:" txt="17052-080" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Endereço:" txt="Av. Getúlio Vargas, 10-130" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Bairro:" txt="Jardim América" />
+            </div>
+            <div className="col-3">
+              <Text titulo="Complemento:" txt="Restaurante" />
+            </div>
+          </div>
+        </div>
 
         <hr />
+
+        <div className="col">
+          <h2 style={{ color: '#666' }}>Formulário</h2>
+          <TxtForm titulo="Plano de saúde:" txt="UNIMED" />
+          <TxtForm titulo="Possui algum tipo de alergia?" txt="Não" />
+          <TxtForm titulo="Toma algum medicamento contínuo?" txt="Não" />
+          <TxtForm
+            titulo="Já realizou alguma cirurgia?"
+            txt="Sim, em 2010 já realizei uma cirurgia bal bla bla da Silva teste"
+          />
+          <TxtForm titulo="Comorbidade:" txt="DOENÇA RENAL CRÔNICA" />
+        </div>
+
+        <hr />
+
         <h2 style={{ color: '#666' }}>Localização</h2>
-        <MapCoordinates longitude={testCoordinates[1].lng} latitude={testCoordinates[1].lat}></MapCoordinates>
+        <MapCoordinates
+          longitude={testCoordinates[1].lng}
+          latitude={testCoordinates[1].lat}
+        ></MapCoordinates>
       </div>
     </section>
   );
