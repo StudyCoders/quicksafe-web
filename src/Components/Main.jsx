e682cbd6f47587a636d4cb7110c732d88e248aa0
caioarai <caio.arai8@gmail.com>
2023-11-22 22:06:02 -0300

    Retorno dos dados

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,34 +1,19 @@
-import React from "react";
-import Text from "../Forms/Text";
-import TxtForm from "../Forms/TxtForm";
-import MapCoordinates from "./MapCoordinates";
-import AsyncSelect from "react-select/async";
+import React from 'react';
+import Text from '../Forms/Text';
+import TxtForm from '../Forms/TxtForm';
+import MapCoordinates from './MapCoordinates';
+import AsyncSelect from 'react-select/async';
 
 const Main = () => {
   const [contato, setContato] = React.useState(null);
   const [formData, setFormData] = React.useState(null);
 
-  const testCoordinates = [
-    {
-      lat: -22.336242338753255,
-      lng: -49.06343452478047,
-    },
-    {
-      lat: -22.3299077,
-      lng: -49.0739861,
-    },
-    {
-      lat: -22.3247475,
-      lng: -49.0327961,
-    },
-  ];
-
   async function fetchDados(acao, body) {
     const url = `https://tecnoatualizados.com/projetos/tcc/api/metodos/formWeb.php?acao=${acao}`;
     const response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
     });
@@ -37,51 +22,59 @@ const Main = () => {
   }
 
   async function loadFormData(dados) {
-    if ( dados ) {
+    if (dados) {
+      setFormData(null);
       let { tipo, codigo } = dados;
-      const data = await fetchDados("formulario", { tipo, codigo });
+      const data = await fetchDados('formulario', { tipo, codigo });
       setFormData(data);
+    } else {
+      setFormData(null);
     }
   }
 
   const loadOptions = async (inputValue) => {
     if (inputValue) {
-      const data = await fetchDados("select", { campo: inputValue, contato });
+      const data = await fetchDados('select', { campo: inputValue, contato });
 
       return data.map((item) => {
         const { codigo, nome, cpf, tipo } = item;
-        const flagTipo = tipo === "U" ? "usuário" : "contato";
+        const flagTipo = tipo === 'U' ? 'usuário' : 'contato';
         const cpfPart = cpf ? ` - ${cpf}` : '';
-  
+
         return {
           value: codigo,
           label: `Código ${flagTipo}: ${codigo} - ${nome}${cpfPart}`,
-          ...item
+          ...item,
         };
       });
     }
   };
 
   return (
-    <section className="animeLeft p-4" style={{ backgroundColor: "#f6f6f6" }}>
+    <section className="animeLeft p-4" style={{ backgroundColor: '#f6f6f6' }}>
       <div className="container">
         <div className="row mb-5">
           <label htmlFor="selectBusca" className="fw-bold">
-            Pesquise pelo{" "}
+            Pesquise pelo{' '}
             <span className="text-decoration-underline">Código do usuário</span>
-            ,{" "}
+            ,{' '}
             <span className="text-decoration-underline">Código do contato</span>
-            , <span className="text-decoration-underline">CPF</span>,{" "}
+            , <span className="text-decoration-underline">CPF</span>,{' '}
             <span className="text-decoration-underline">
               Número do telefone
-            </span>{" "}
-            ou{" "}
+            </span>{' '}
+            ou{' '}
             <span className="text-decoration-underline">
               Nome do solicitante
             </span>
           </label>
           <div className="col">
-            <AsyncSelect isClearable loadOptions={loadOptions} onChange={loadFormData} placeholder="Comece a digitar..." />
+            <AsyncSelect
+              isClearable
+              loadOptions={loadOptions}
+              onChange={loadFormData}
+              placeholder="Comece a digitar..."
+            />
           </div>
           <div className="col-12 mt-2">
             <div className="form-check form-switch">
@@ -99,118 +92,132 @@ const Main = () => {
           </div>
         </div>
 
-        {
-          /* Apenas para exemplo */
-          formData ? (
-            <div className="row mb-3">
-              {JSON.stringify(formData)}
-            </div>
-          ) : null
-        }
-
-        <div className="col">
-          <h2 style={{ color: "#666" }}>Informações do Usuário</h2>
-          <div className="row mb-3">
-            <div className="col-4">
-              <Text titulo="Código do Usuário:" txt="117" />
-            </div>
-            <div className="col-4">
-              <Text titulo="Nome do usuário:" txt="CAIO TESTE" />
-            </div>
-            <div className="col-4">
-              <Text titulo="E-mail:" txt="caio@gmail.com" />
-            </div>
-          </div>
-          <div className="row mb-3">
-            <div className="col-4">
-              <Text titulo="CPF:" txt="000.111.720-00" />
-            </div>
-            <div className="col-4">
-              <Text titulo="Data de Nascimento:" txt="28/06/2002" />
-            </div>
-            <div className="col-4">
-              <Text titulo="Sexo:" txt="Masculino" />
-            </div>
-          </div>
-
-          <div className="col">
-            <h3 style={{ color: "#666" }}>Endereço</h3>
-            <div className="row mb-3">
-              <div className="col-3">
-                <Text titulo="Telefone:" txt="-" />
+        {formData && (
+          <>
+            <div className="col">
+              <h2 className="subtitulo">Informações do Usuário</h2>
+              <div className="row mb-3">
+                <div className="col-4">
+                  <Text titulo="Código do Usuário:" txt={formData.id_usuario} />
+                </div>
+                <div className="col-4">
+                  <Text titulo="Nome do usuário:" txt={formData.nome_usuario} />
+                </div>
+                <div className="col-4">
+                  <Text titulo="E-mail:" txt={formData.email_usuario} />
+                </div>
               </div>
-              <div className="col-3">
-                <Text titulo="Celular:" txt="(11) 99999-9999" />
-              </div>
-              <div className="col-3">
-                <Text titulo="Estado:" txt="SÃO PAULO" />
-              </div>
-              <div className="col-3">
-                <Text titulo="Cidade:" txt="BAURU" />
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-3">
-                <Text titulo="CEP:" txt="17052-080" />
-              </div>
-              <div className="col-3">
-                <Text titulo="Endereço:" txt="Av. Getúlio Vargas, 10-130" />
-              </div>
-              <div className="col-3">
-                <Text titulo="Bairro:" txt="Jardim América" />
-              </div>
-              <div className="col-3">
-                <Text titulo="Complemento:" txt="Restaurante" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <hr />
-
-        {contato && (
-          <div className="col">
-            <h2 style={{ color: "#666" }}>Informações do Contato</h2>
-            <div className="row mb-3">
-              <div className="col-4">
-                <Text titulo="Código do Contato:" txt="2" />
-              </div>
-              <div className="col-4">
-                <Text titulo="Nome do contato:" txt="MÃE" />
-              </div>
-              <div className="col-4">
-                <Text titulo="CPF:" txt="-" />
+              <div className="row mb-3">
+                <div className="col-4">
+                  <Text titulo="CPF:" txt={formData.cpf_usuario} />
+                </div>
+                <div className="col-4">
+                  <Text
+                    titulo="Data de Nascimento:"
+                    txt={formData.dt_nascimento}
+                  />
+                </div>
+                <div className="col-4">
+                  <Text titulo="Sexo:" txt={formData.tp_sexo} />
+                </div>
               </div>
             </div>
 
             <hr />
-          </div>
+
+            {contato && (
+              <div className="col">
+                <h2 className="subtitulo">Informações do Contato</h2>
+                <div className="row mb-3">
+                  <div className="col-4">
+                    <Text
+                      titulo="Código do Contato:"
+                      txt={formData.id_contato}
+                    />
+                  </div>
+                  <div className="col-4">
+                    <Text
+                      titulo="Nome do contato:"
+                      txt={formData.nome_contato}
+                    />
+                  </div>
+                  <div className="col-4">
+                    <Text titulo="CPF:" txt={formData.cpf_contato} />
+                  </div>
+                </div>
+
+                <hr />
+              </div>
+            )}
+
+            <div className="col">
+              <h2 className="subtitulo">
+                Formulário do {contato ? 'Contato' : 'Usuário'}
+              </h2>
+              <div className="col">
+                <div className="row mb-3">
+                  <div className="col-3">
+                    <Text titulo="Telefone:" txt={formData.telefone} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Celular:" txt={formData.celular} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Estado:" txt={formData.estado} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Cidade:" txt={formData.cidade} />
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <div className="col-3">
+                    <Text titulo="CEP:" txt={formData.cep} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Endereço:" txt={formData.endereco} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Bairro:" txt={formData.bairro} />
+                  </div>
+                  <div className="col-3">
+                    <Text titulo="Complemento:" txt={formData.complemento} />
+                  </div>
+                </div>
+              </div>
+              <TxtForm titulo="Plano de saúde:" txt={formData.plano_saude} />
+              <TxtForm
+                titulo="Possui algum tipo de alergia?"
+                txt={formData.alergia}
+              />
+              <TxtForm
+                titulo="Toma algum medicamento contínuo?"
+                txt={formData.med_continuo}
+              />
+              <TxtForm
+                titulo="Já realizou alguma cirurgia?"
+                txt={formData.cirurgia}
+              />
+              <TxtForm titulo="Comorbidade:" txt={formData.comorbidade} />
+            </div>
+
+            <hr />
+
+            <div className="col">
+              <h2 className="subtitulo">Localização</h2>
+              {formData.longitude ? (
+                <>
+                  <p>Última localização do usuário - {formData.dthr_local}</p>
+                  <MapCoordinates
+                    longitude={formData.longitude}
+                    latitude={formData.latitude}
+                  />
+                </>
+              ) : (
+                'Não localizada'
+              )}
+            </div>
+          </>
         )}
-
-        <div className="col">
-          <h2 style={{ color: "#666" }}>
-            Formulário do {contato ? "Contato" : "Usuário"}
-          </h2>
-          <TxtForm titulo="Plano de saúde:" txt="UNIMED" />
-          <TxtForm titulo="Possui algum tipo de alergia?" txt="Não" />
-          <TxtForm titulo="Toma algum medicamento contínuo?" txt="Não" />
-          <TxtForm
-            titulo="Já realizou alguma cirurgia?"
-            txt="Sim, em 2010 já realizei uma cirurgia bal bla bla da Silva teste"
-          />
-          <TxtForm titulo="Comorbidade:" txt="DOENÇA RENAL CRÔNICA" />
-        </div>
-
-        <hr />
-
-        <div className="col">
-          <h2 style={{ color: "#666" }}>Localização</h2>
-          <p>Última localização - 18/11/2023 18:05:36</p>
-          <MapCoordinates
-            longitude={testCoordinates[1].lng}
-            latitude={testCoordinates[1].lat}
-          ></MapCoordinates>
-        </div>
       </div>
     </section>
   );
